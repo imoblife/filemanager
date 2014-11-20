b4ad1c852c2967e44de7ab39bd87003c6fc455b2
huxugang <252742941@qq.com>
2014-11-20 18:19:53 +0800

    File 添加搜索，文件夹列出文件数 Conflicts: 	FileManager/project.properties

--- FileManager/src/com/filemanager/search/SearchListAdapter.java
+++ FileManager/src/com/filemanager/search/SearchListAdapter.java
@@ -3,13 +3,14 @@ package com.filemanager.search;
 import java.io.File;
 import java.util.HashMap;
 
-
 import com.filemanager.R;
+import com.filemanager.ThumbnailLoader;
 import com.filemanager.files.FileHolder;
 import com.filemanager.view.ViewHolder;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,21 +20,26 @@ import android.widget.TextView;
 
 /**
  * Simple adapter for displaying search results.
+ * 
  * @author George Venios
- *
+ * 
  */
 public class SearchListAdapter extends CursorAdapter {
 	private HashMap<String, FileHolder> itemCache = new HashMap<String, FileHolder>();
 
 	public SearchListAdapter(Context context, Cursor c) {
+
 		super(context, c, true);
 	}
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		String path = cursor.getString(cursor.getColumnIndex(SearchResultsProvider.COLUMN_PATH));
+		String path = cursor.getString(cursor
+				.getColumnIndex(SearchResultsProvider.COLUMN_PATH));
+
 		FileHolder fHolder;
-		if((fHolder = itemCache.get(path)) == null){
+
+		if ((fHolder = itemCache.get(path)) == null) {
 			fHolder = new FileHolder(new File(path), context);
 			itemCache.put(path, fHolder);
 		}
@@ -41,7 +47,6 @@ public class SearchListAdapter extends CursorAdapter {
 		ViewHolder h = (ViewHolder) view.getTag();
 		h.primaryInfo.setText(fHolder.getName());
 		h.secondaryInfo.setText(path);
-		h.icon.setImageDrawable(fHolder.getIcon());
 	}
 
 	@Override
@@ -49,17 +54,16 @@ public class SearchListAdapter extends CursorAdapter {
 		// Inflate the view
 		ViewGroup v = (ViewGroup) ((LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
-				R.layout.item_filelist, null);
+				R.layout.serch_item_filelist, null);
 
 		// Set the viewholder optimization.
 		ViewHolder holder = new ViewHolder();
-		holder.icon = (ImageView) v.findViewById(R.id.icon);
 		holder.primaryInfo = (TextView) v.findViewById(R.id.primary_info);
 		holder.secondaryInfo = (TextView) v.findViewById(R.id.secondary_info);
 		v.findViewById(R.id.tertiary_info).setVisibility(View.GONE);
-		
+
 		v.setTag(holder);
-		
+
 		return v;
 	}
 }
